fba628eac1be1a3c3fd93d08afc2c620051fd303
malzahnparker <malzahnparker@outlook.com>
2025-02-05 17:47:54 +0000

    E

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use rand::distr::{Distribution, Uniform};
 use rand::thread_rng;
 
+const LEARNING_RATE: f32 = 0.01;
+
 struct NeuralNetworkLayer {
     weights: Vec<Vec<f32>>,
     biases: Vec<f32>,
     num_inputs: usize,
     num_outputs: usize,
+    inputs: Vec<f32>,
     outputs: Vec<f32>,
+    errors: Vec<f32>,
 }
 
 impl NeuralNetworkLayer {
@@ -25,11 +29,14 @@ impl NeuralNetworkLayer {
             biases: biases,
             num_inputs: num_inputs,
             num_outputs: num_outputs,
+            inputs: vec![0.0; num_inputs]
             outputs: vec![0.0; num_outputs],
+            errors: vec![o.0; num_outputs]
         }
 
     }
     fn forward(&mut self, inputs: &[f32]) {    
+        self.inputs = inputs.clone();
         for i in 0..self.num_outputs {
             self.outputs[i] = 0.0;
             for j in 0..self.num_inputs {
@@ -39,15 +46,21 @@ impl NeuralNetworkLayer {
             self.outputs[i] = self.sigmoid(self.outputs[i]);
         }
     }
-    /*fn backwards(previous_errors: Vec<f32>) {
-        let mut hidden_errors = vec![0.0; HIDDEN_SIZE];
-        for j in 0..HIDDEN_SIZE {
-            for k in 0..OUTPUT_SIZE {
-                hidden_errors[j] += output_errors[k] * self.weights_hidden_output[j][k];
+    fn backwards(&mut self, previous_errors: Vec<f32>, num_previous: usize) {
+        let mut self.errors = vec![0.0; self.num_outputs];
+        for j in 0..num_previous {
+            for k in 0..num_previous {
+                self.errors[j] += previous_errors[k] * self.weights[j][k];
             }
-            hidden_errors[j] *= sigmoid_derivative(self.hidden_layer[j]);
+            self.errors[j] *= sigmoid_derivative(self.outputs[j]);
         }
-    }*/
+        for j in 0..self.num_outputs {
+            for i in 0..self.num_inputs {
+                self.weights[i][j] += LEARNING_RATE * self.errors[j] * self.inputs[i];
+            }
+            self.biases[j] += LEARNING_RATE * self.errors[j];
+        }
+    }
     fn sigmoid(&self, x: f32) -> f32 {
         1.0 / (1.0 + (-x).exp())
     }
@@ -66,5 +79,7 @@ fn main() {
     dense1.forward(&inputs);
     dense2.forward(&dense1.outputs);
     dense3.forward(&dense2.outputs);
-    dbg!("{}", dense3.outputs);
+    dbg!(dense3.outputs);
+    let errors = vec![1.0 - dense1.outputs[0], ];
+    dense3.backwards(vec![1.0 - dense1.outputs])
 }
